65ea40df46de0f23d593b157a83c5eadd4a9f1b3
hushwa <joshuaravelo5553.JR@gmail.com>
2025-05-20 08:58:33 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Music-PLayer-App',
+  webDir: 'www'
+};
+
+export default config;
